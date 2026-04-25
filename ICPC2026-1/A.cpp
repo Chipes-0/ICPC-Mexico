bfebd8f58f98ea1d6bf8d4fd0b02acc4b3485593
Chipes <carlos1227a@outlook.com>
2026-04-25 12:33:31 -0600

    docs: documentar problema A

--- ICPC2026-1/A.cpp
+++ ICPC2026-1/A.cpp
@@ -1,4 +1,33 @@
-// https://codeforces.com/gym/106495/problem/A
+/*
+PROBLEM: https://codeforces.com/gym/106495/problem/A
+TIME_COMPLEXITY: O(N + M)
+SPACE_COMPLEXITY: O(1)
+
+INPUT:
+Test case 1:
+5 4
+180 250 120 300 150
+200 180 150 220
+
+OUTPUT:
+NO
+
+Test case 2:
+4 5
+200 350 250 200
+220 210 200 190 230
+
+OUTPUT:
+NO
+
+Test case 3:
+3 3
+300 350 250
+350 400 260
+
+OUTPUT:
+YES
+*/
 
 #include <bits/stdc++.h>
 
